c57c892735651e2a88f87ebbc6187e90c79c01cf
Dualcube <Dualcube@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-06 14:53:58 +0000

    free order issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/wc-sequential-order-numbers/tags/1.1.1@1193418 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config_sequencial_order_number.php
@@ -0,0 +1,8 @@
+<?php
+
+define('WC_SEQUENTIAL_ORDER_NUMBERS_PLUGIN_TOKEN', 'wc-Sequential-Order-Numbers');
+
+define('WC_SEQUENTIAL_ORDER_NUMBERS_TEXT_DOMAIN', 'Sequential_Order_Numbers');
+
+define('WC_SEQUENTIAL_ORDER_NUMBERS_PLUGIN_VERSION', '1.0.0');
+?>
